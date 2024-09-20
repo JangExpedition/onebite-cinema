4c8fd8a6041863cf318037fcdb3e855799970b6d
jang_expedition <jangexpedition@gmail.com>
2024-09-20 22:02:15 +0900

    SEO 이미지 경로 수정

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -28,7 +28,7 @@ export default function Page() {
     <>
       <Head>
         <title>한입씨네마 - 검색결과</title>
-        <meta property="og:image" content="/thumnail.png" />
+        <meta property="og:image" content="/thumbnail.png" />
         <meta property="og:title" content="한입씨네마" />
         <meta
           property="og:description"
